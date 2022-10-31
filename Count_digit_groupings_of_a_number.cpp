250a8a47c61d82e1559997dca72865c440dd574f
tttttat <91891505+tttttat@users.noreply.github.com>
2022-10-31 21:35:15 +0530

    Add files via upload

---
+++ Count_digit_groupings_of_a_number.cpp
@@ -0,0 +1,2 @@
+/*Given a string str consisting of digits, one may group these digits into sub-groups while maintaining their original order.
+The task is to count number of groupings such that for every sub-group except the last one, sum of digits in a sub-group is less than or equal to sum of the digits in the sub-group immediately on its right.*/
