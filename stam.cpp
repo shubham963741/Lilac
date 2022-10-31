b8093136a7ffcb87b0528fe15bdd97d1cb121996
G. Sainath <81852168+blakestiles@users.noreply.github.com>
2022-10-31 20:54:13 +0530

    Add files via upload

---
+++ stam.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>  
+class static_type {  static int i;
+public:
+static void init(int x) {i = x;} 
+ void show() {cout << i;}
+};
+int static_type::i; // define i 
+ int main()
+{
+// init static data before object creation  static_type::init(100);
+
+static_type x;
+x.show(); // displays 100
+
+return 0;
+}
